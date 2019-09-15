8b0290d2e8522a7dda19d4f5e3263abb07e05ce2
Dan Park <dpark.sports@gmail.com>
2019-09-15 13:23:06 -0700

    Add files via upload

---
+++ sse-avx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#CPU flag detection
+echo "****Getting CPU flag capabilities and number of cores"
+cat /proc/cpuinfo  | egrep "(flags|model name|vendor)" | sort | uniq -c
+#Compiler capabilities. -march=native is required!
+echo "****Getting GCC capabilities"
+gcc -march=native -dM -E - < /dev/null | egrep "SSE|AVX" | sort 
+#OS kernel version
+echo "****Getting OS Kernel Version"
+uname -a
